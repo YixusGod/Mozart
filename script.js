739c533237c0c33b2304775fb86b69df4367be06
Yixus <jj.justiesp@gmail.com>
2024-06-19 12:16:21 -0400

    pequeños cambios 2

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const closet = 2024
-// para guardar constanstante o variable se utiliza el comando cons
+// const tipoNumber = 2024
+// // para guardar constanstante o variable se utiliza el comando cons junto con el nombre de la variable. los tipo number son cons que contienen numero
+// const tipoString = "holi" // este tipo de constante tipo son de texto
+// const tipoBoolean = true // el tipo de dato booleano es aquel que maneja verdadero o falso
+// const tipoUndefinet = undefined // sinonimo de error
+// const tipoNull = null // que no existe pero no signica que haya algun error 
+// const tipoArray = ["mundo, holi"] // tipo lista 
 
-console.log(closet)
+// console.log(tipoArray.concat(tipoString))
 
+// const tipoObjeto = {
+//     raza: "kacri",
+//     edad: 10,
+//     dueno: "carlos",
+//     hambre:false,
+// } 
+
+// const funciontipoflecha = () => {
+//     //aqui va el codigo
+// }
+
+axios.get("https://leonardoapi.onrender.com/songs").then((res) => {
+    //guardo la lista de canciones
+    const songList = res.data.songs
+
+    //recorriendo la lista de canciones
+    songList.map((song) => {
+        console.log(song)
+    })
+})
